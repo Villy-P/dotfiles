3382d2d75e14b81b2e816c5be274f0dc059cdf56
Valerius Petrini <valeriuspetrini@gmail.com>
2026-07-26 10:33:48 -0500

    feat: refactor gen_pkg_info.sh to use consistent variable for PACKAGE_TOML

--- scripts/gen_pkg_info.sh
+++ scripts/gen_pkg_info.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-PACKAGE_LIST=~/dotfiles/packages/packages.toml
+PACKAGE_DIR=~/dotfiles/packages
+PACKAGE_TOML="$PACKAGE_DIR/packages.toml"
 
-pacman -Qqen > ~/dotfiles/packages/pacman.txt
-pacman -Qqem > ~/dotfiles/packages/aur.txt
+pacman -Qqen > "$PACKAGE_DIR/pacman.txt"
+pacman -Qqem > "$PACKAGE_DIR/aur.txt"
 
 yay -Qqe | while IFS= read -r package; do
-    grep -q "^$package" $PACKAGE_LIST || echo "$package" >> $PACKAGE_LIST
+    grep -q "^$package" $PACKAGE_TOML || echo "$package" >> $PACKAGE_TOML
 done
