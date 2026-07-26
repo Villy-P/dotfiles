a9a449468116f28190df9afc480900b3bbc1d0ce
Valerius Petrini <valeriuspetrini@gmail.com>
2026-07-26 10:19:50 -0500

    feat: add packages.toml support to gen_pkg_info.sh

--- scripts/gen_pkg_info.sh
+++ scripts/gen_pkg_info.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+PACKAGE_LIST=~/dotfiles/packages/packages.toml
+
 pacman -Qqen > ~/dotfiles/packages/pacman.txt
 pacman -Qqem > ~/dotfiles/packages/aur.txt
+
+yay -Qqe | while IFS= read -r package; do
+    grep -q "^$package" $PACKAGE_LIST || echo "$package" >> $PACKAGE_LIST
+done
