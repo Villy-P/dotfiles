9e03fe34417c2c964606e2b6fb739467b3f52242
Valerius Petrini <valeriuspetrini@gmail.com>
2026-07-26 22:54:14 -0500

    feat: add additional monitor configuration for eDP-1

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -34,6 +34,12 @@ hl.monitor({
     position  = "auto",
     scale     = 1,
 })
+hl.monitor({
+    output    = "eDP-1",
+    mode      = "preferred",
+    position  = "auto",
+    scale     = 1
+})
 
 ---------------------
 ---- MY PROGRAMS ----
