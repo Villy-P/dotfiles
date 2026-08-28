e4ca3af5cace81bc0a71745a1b0ded4654d699fa
Valerius Petrini <valeriuspetrini@gmail.com>
2026-08-27 21:43:53 -0500

    feat: add openrgb stuff

--- scripts/setwall_rand.sh
+++ scripts/setwall_rand.sh
@@ -9,8 +9,7 @@ if [ ${#files[@]} -gt 0 ]; then
     # Pick a random index based on array length
     wall="${files[RANDOM % ${#files[@]}]}"
     echo "Picking Wallpaper: $wall"
-    awww img "$wall" --transition-type fade --transition-duration 1
-    matugen image "$wall" --mode dark --source-color-index 0
+    ~/dotfiles/scripts/setwall.sh $wall
 else
     echo "The directory is empty."
 fi
