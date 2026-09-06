aed7ff295eb47cc1c1e57fc73c6eb4d8e180869d
Valerius Petrini <valeriuspetrini@gmail.com>
2026-09-06 00:33:22 -0500

    feat(packages): add langauge servers for corekit ui

--- stow/neovim/.config/nvim/lua/config/lsp.lua
+++ stow/neovim/.config/nvim/lua/config/lsp.lua
@@ -1,22 +1,31 @@
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file("", true),
-        checkThirdParty = false
-      }
-    }
-  }
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+		},
+	},
 })
 
+vim.lsp.config("ts_ls", { capabilities = capabilities })
+vim.lsp.config("svelte", { capabilities = capabilities })
+vim.lsp.config("tailwindcss", { capabilities = capabilities })
+
 vim.lsp.enable("lua_ls")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("svelte")
+vim.lsp.enable("tailwindcss")
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition)
 vim.keymap.set("n", "K", vim.lsp.buf.hover)
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float)
--- stow/neovim/.config/nvim/lua/plugins/init.lua
+++ stow/neovim/.config/nvim/lua/plugins/init.lua
@@ -1,64 +1,101 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "master",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "vim", "vimdoc" },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "master",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "lua", "vim", "vimdoc", "typescript", "tsx", "svelte" },
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 
-  {
-    "pocco81/auto-save.nvim",
-    config = function()
-      require("auto-save").setup({
-        enabled = true,
-        trigger_events = { "InsertLeave", "TextChanged" },
-      })
-    end,
-  },
-  
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>" },
-      { "<leader>fb", "<cmd>Telescope buffers<cr>" },
-    },
-  },
+	{
+		"pocco81/auto-save.nvim",
+		config = function()
+			require("auto-save").setup({
+				enabled = true,
+				trigger_events = { "InsertLeave", "TextChanged" },
+			})
+		end,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<leader>e", "<cmd>NvimTreeToggle<cr>" },
-    },
-    config = function()
-      require("nvim-tree").setup({})
-    end,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		keys = {
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>" },
+		},
+	},
 
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  
-  {
-    "neovim/nvim-lspconfig"
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{ "<leader>e", "<cmd>NvimTreeToggle<cr>" },
+		},
+		config = function()
+			require("nvim-tree").setup({})
+		end,
+	},
 
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require("nvim-autopairs").setup({})
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+
+	{
+		"neovim/nvim-lspconfig",
+	},
+
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	},
+
+	{
+		{
+			"stevearc/conform.nvim",
+			event = "BufWritePre",
+			config = function()
+				require("conform").setup({
+					formatters_by_ft = {
+						lua = { "stylua" },
+					},
+					format_on_save = {
+						timeout_ms = 500,
+						lsp_fallback = true,
+					},
+				})
+			end,
+		},
+	},
+
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("tokyonight")
+		end,
+	},
+
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			require("config.cmp")
+		end,
+	},
 }
